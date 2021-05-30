cba07c2832923782290852b89fc5403f323a1b40
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-05-29 17:49:58 -0700

    Create position.js

---
+++ position.js
@@ -0,0 +1,18 @@
+const finalPosition = function (moves) {
+  let position = [0, 0];
+  for (const move of moves) {
+    if (move === "north") {
+      position[1]++;
+    } else if (move === "south") {
+      position[1]--;
+    } else if (move === "west") {
+      position[0]--;
+    } else {
+      position[0]++;
+    }
+  }
+} 
+
+const moves = ['north', 'north', 'west', 'west', 'north', 'east','north']  
+
+console.log(finalPosition(moves));
