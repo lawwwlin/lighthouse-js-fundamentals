5b3d850778b94c590484d5980a0361150c39ccb2
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-05-29 20:42:08 -0700

    Update trash.js

--- trash.js
+++ trash.js
@@ -1,3 +1,9 @@
+var bins = {
+  waste: 0,
+  recycling: 0,
+  compost: 0,
+}
+
 const smartGarbage = function (trash, bins) {
   if (trash == "waste") {
     bins.waste++;
@@ -8,11 +14,7 @@ const smartGarbage = function (trash, bins) {
   }
 }
 
-var bins = {
-  waste: 0,
-  recycling: 0,
-  compost: 0,
-}
+
 smartGarbage('recycling', { waste: 4, recycling: 2, compost: 5 })
 console.log(bins.waste);
 console.log(bins.recycling);
