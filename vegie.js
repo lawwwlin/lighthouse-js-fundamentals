c5436cbb2aec4fd4e8530ea898e05f310c9fd50c
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-05-29 21:08:49 -0700

    Create vegie.js

---
+++ vegie.js
@@ -0,0 +1,47 @@
+const judgeVegetable = function (vegetables, metric) {
+  let vegie = { submitter: "", redness: 0, plumpness: 0 }
+  if (metric == "redness") {
+    let r = 0
+    for (veg in vegetables) {
+      if (vegetables[veg].redness > r) {
+        r = vegetables[veg].redness;
+        vegie.submitter = vegetables[veg].submitter;
+        vegie.redness = vegetables[veg].redness;
+        vegie.plumpness = vegetables[veg].plumpness;
+      }
+    }
+  } else if (metric == "plumpness") {
+    let p = 0
+    for (veg in vegetables) {
+      if (vegetables[veg].plumpness > p) {
+        p = vegetables[veg].plumpness;
+        vegie.submitter = vegetables[veg].submitter;
+        vegie.redness = vegetables[veg].redness;
+        vegie.plumpness = vegetables[veg].plumpness;
+      }
+    }
+  }
+}
+
+const vegetables = [
+  {
+    submitter: 'Old Man Franklin',
+    redness: 10,
+    plumpness: 5
+  },
+  {
+    submitter: 'Sally Tomato-Grower',
+    redness: 2,
+    plumpness: 8
+  },
+  {
+    submitter: 'Hamid Hamidson',
+    redness: 4,
+    plumpness: 3
+  }
+]
+
+const metric = 'redness'
+
+judgeVegetable(vegetables, metric)
+
