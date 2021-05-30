320d45d2ac9f9ddc319f0aff6be1f06529e8dfc5
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-05-29 18:40:07 -0700

    Update area.js

--- area.js
+++ area.js
@@ -18,7 +18,7 @@ const calculateCircleArea =  function (radius) {
   if (radius < 0) {
     return undefined;
   } else {
-    return Math.pow(Math.PI * radius, 2);
+    return Math.PI * Math.pow(radius, 2);
   }
 }
 
