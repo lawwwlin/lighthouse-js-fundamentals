5903a48e1f570e00c5e6c1d5b2081690fa572b4b
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-05-29 20:57:55 -0700

    Create cars.js

---
+++ cars.js
@@ -0,0 +1,24 @@
+const carPassing = function (cars, speed) {
+  let car = { time: Date.now, s: speed }
+  cars.push(car);
+}
+
+const cars = [
+  {
+    time: 1568329654807,
+    speed: 40,
+  },
+  {
+    time: 1568329821632,
+    speed: 42,
+  },
+  {
+    time: 1568331115463,
+    speed: 35
+  }
+]
+
+const speed = 38
+
+console.log(carPassing(cars, speed));
+
