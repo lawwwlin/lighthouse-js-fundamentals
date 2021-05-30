ca869ee9a2b9d26b32a72df31f0cfb454b523b94
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-05-29 19:48:53 -0700

    Update range.js

--- range.js
+++ range.js
@@ -4,7 +4,7 @@ const range = function (start, end, step) {
   if (start <= 0) {
     array = [];
   } else {
-    for (let i = start; i <= end; i++) {
+    for (let i = start; i <= end; step) {
       array.push(i);
     }
   }
