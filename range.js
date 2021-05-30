387bfe20e121116436eb89fcd65beacd800bdf07
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-05-29 19:32:08 -0700

    Update range.js

--- range.js
+++ range.js
@@ -1,11 +1,6 @@
 const range = function (start, end, step) {
   let array = [];
-  if (isNaN(start) || isNaN(end) || isNaN(step)) {
-    if (start > end) {
-      if (step <= 0) {
-        array = [];
-      }
-    }
+  if (isNaN(start) || isNaN(end) || isNaN(step) || start > end || step <= 0) {
   } else {
     for (let i = start; i <= end; i + step) {
       array.push(i);
