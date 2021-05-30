21e88432e8e18fd7450c68e16d4951816c95556d
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-05-29 18:58:37 -0700

    Update ice-cream.js

--- ice-cream.js
+++ ice-cream.js
@@ -7,5 +7,3 @@ console.log(iceCreamFlavours);
 console.log(iceCreamFlavours[0]);
 console.log(iceCreamFlavours[iceCreamFlavours.length - 1]);
 console.log(iceCreamFlavours.length);
-console.log(5 + "3")
-console.log(5 % "3")
