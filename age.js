b79af3dc5202b02ffd67d8ff17f4475c2a0a37ad
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-05-29 18:03:40 -0700

    Update age.js

--- age.js
+++ age.js
@@ -1,7 +1,7 @@
- const ageCalculator = function (name, yearOfBirth, currentYear) {
-  age = currentYear - yearOfBirth;
+const ageCalculator = function (name, yearOfBirth, currentYear) {
+  const age = currentYear - yearOfBirth;
   return name + " is " + age + " years old."
- }
+}
 
 console.log(ageCalculator("Miranda", 1983, 2015));
 console.log(ageCalculator("Ferdinand", 1988, 2015));
