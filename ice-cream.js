b00b2b0a9aee18ecada4e9d71c4628b9a2b22d75
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-05-29 18:47:56 -0700

    Update ice-cream.js

--- ice-cream.js
+++ ice-cream.js
@@ -1,8 +1,9 @@
 // Do not modify these first two lines
 const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
 console.log(iceCreamFlavours);
+// Your code below here...
 iceCreamFlavours.push("root beer");
 console.log(iceCreamFlavours);
 console.log(iceCreamFlavours[0]);
 console.log(iceCreamFlavours[iceCreamFlavours.length-1]);
-// Your code below here...
+console.log(iceCreamFlavours.length);
